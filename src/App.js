d50df537fe14f382ea766dc9fa0d714085af4be5
Anna <anna@bpf-107.local>
2020-12-10 13:01:53 +0100

    react_ion

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Home from './components/home/Home';
+import About from './components/about/About';
+
+function App() {
+  return (
+    <div className="App">
+     <Home />
+     <About />
+    </div>
+  );
+}
+
+export default App;
